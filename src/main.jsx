0bf5f40eeb2c16e27f570ecd4a10cc9572edd90b
Siddami <dharmmy621@gmail.com>
2026-02-16 13:14:31 +0100

    feat: setup global css and installed inter font
    - I choose to install inter font using @fontsource,
    becuase of it's performance, its offline access and faster
    loading time.
    
    - global css file is created to set the font family for the entire
    application. themes , colors and overwrite some defaults

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import '@fontsource/inter/300.css'  // Light
+import '@fontsource/inter/400.css'  // Regular
+import '@fontsource/inter/500.css'  // Medium
+import '@fontsource/inter/600.css'  // Semi-bold
+import '@fontsource/inter/700.css'  // Bold
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
