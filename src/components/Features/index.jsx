9ab031bd68a60f02726e3eda60f4c6dab489b85d
Siddami <dharmmy621@gmail.com>
2026-02-17 15:02:42 +0100

    feat: added features section to home page

---
+++ src/components/Features/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <section className="py-20 bg-white" aria-label="Features Section">
+      <div className="text-center mx-auto px-4">
+        <h2 className="text-3xl md:text-5xl font-bold text-primary mb-4">
+          Smart Tools at a Glance
+        </h2>
+        <p className="text-primary-text max-w-lg mx-auto">
+          Everything you need to track, predict and stay right on top of your
+          pharmacy stock
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Features
